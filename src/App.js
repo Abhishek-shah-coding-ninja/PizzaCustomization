920062705bd892b7fa9970dc535aa8550e25ebaf
abhishek-shah-vst-au4 <54413214+abhishek-shah-vst-au4@users.noreply.github.com>
2020-09-01 23:46:43 +0530

    OnChange Function for changing state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Customize from "./components/Customize"
 
 function App() {
+  const [ingredients, setIngredients] =useState({
+    
+    basil:false,
+    cheese:false,
+    mushroom:false,
+    olive:false,
+    pineapple:false,
+    tomato: false,
+
+  });
   return (
     <div className="App">
       <Header />
       <Router>
         <Switch>
           <Route exact path="/">
-            <Customize/>
+            <Customize ingredients={ingredients} setIngredients={setIngredients}/>
           </Route>
           <Route path="/checkout">
             <h1>Checkout</h1>
--- src/components/Customize.js
+++ src/components/Customize.js
@@ -1,10 +1,143 @@
 import React from "react";
 
-function Customize() {
+import Cheese from "../assets/BaseCheese.png";
+import Base from "../assets/PizzaBase.png";
+import Olive from "../assets/Olive.png";
+import Pineapple from "../assets/Pineapple.png";
+import Mushroom from "../assets/Mushroom.png";
+import Basil from "../assets/Basil.png";
+import Tomato from "../assets/Tomato.png";
+
+function Customize({ingredients , setIngredients}) {
+
+    const onChange = (event, name) => {
+        console.log(localStorage);
+        let newIngredients = JSON.parse(JSON.stringify(ingredients));
+        newIngredients[name] = event;
+        setIngredients(newIngredients);
+        localStorage.setItem("ingredients", JSON.stringify(newIngredients));
+      };
+
   return (
     <div style={{ display: "flex" }}>
-      <div style={{ border: "2px solid black", flex: 1 }}>image</div>
-      <div style={{ border: "2px solid black", flex: 1 }}>Checkbox</div>
+        {JSON.stringify(ingredients)}
+      <div style={{ border: "2px solid black", flex: 1 }}>
+        <div style={{ maxHeight: 500, maxWidth: 500, position: "relative" }}>
+          <img
+            src={Cheese}
+            alt="Pizza Base"
+            height="100%"
+            width="100%"
+            className="ingredients"
+          />
+          <img
+            src={Olive}
+            alt="Pizza Base"
+            height="100%"
+            width="100%"
+            className="ingredients"
+          />
+          <img
+            src={Pineapple}
+            alt="Pizza Base"
+            height="100%"
+            width="100%"
+            className="ingredients"
+          />
+          <img
+            src={Mushroom}
+            alt="Pizza Base"
+            height="100%"
+            width="100%"
+            className="ingredients"
+          />
+          <img
+            src={Basil}
+            alt="Pizza Base"
+            height="100%"
+            width="100%"
+            className="ingredients"
+          />
+          <img
+            src={Tomato}
+            alt="Pizza Base"
+            height="100%"
+            width="100%"
+            className="ingredients"
+          />
+          <img src={Base} alt="Pizza Base" height="100%" width="100%" />
+        </div>
+      </div>
+      <div style={{ border: "2px solid black", flex: 1 }}>
+      <label className="container-checkbox">
+          Pineapple
+          <input
+            type="checkbox"
+            checked={ingredients["pineapple"]}
+            onChange={(event) =>
+              onChange(event.currentTarget.checked, "pineapple")
+            }
+          />
+          <span className="checkmark"></span>
+        </label>
+        <label className="container-checkbox">
+          Basil
+          <input
+            type="checkbox"
+            checked={ingredients["basil"]}
+            onChange={(event) => onChange(event.currentTarget.checked, "basil")}
+          />
+          <span className="checkmark"></span>
+        </label>
+        <label className="container-checkbox">
+          Olive
+          <input
+            type="checkbox"
+            checked={ingredients["olive"]}
+            onChange={(event) => onChange(event.currentTarget.checked, "olive")}
+          />
+          <span className="checkmark"></span>
+        </label>
+        <label className="container-checkbox">
+          Tomato
+          <input
+            type="checkbox"
+            checked={ingredients["tomato"]}
+            onChange={(event) =>
+              onChange(event.currentTarget.checked, "tomato")
+            }
+          />
+          <span className="checkmark"></span>
+        </label>
+        <label className="container-checkbox">
+          Mushroom
+          <input
+            type="checkbox"
+            checked={ingredients["mushroom"]}
+            onChange={(event) =>
+              onChange(event.currentTarget.checked, "mushroom")
+            }
+          />
+          <span className="checkmark"></span>
+        </label>
+        <label className="container-checkbox">
+          Cheese
+          <input
+            type="checkbox"
+            checked={ingredients["cheese"]}
+            onChange={(event) =>
+              onChange(event.currentTarget.checked, "cheese")
+            }
+          />
+          <span className="checkmark"></span>
+        </label>
+        {/* <button
+          onClick={() => history.push("/checkout")}
+          className="proceedToCheckout"
+        >
+          Proceed to Checkout
+        </button> */}
+      </div>
     </div>
   );
 }
